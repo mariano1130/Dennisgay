6e4fa1651c558b6ecb602df260bcffaad9fe42a5
Dennis <dnem3011@gmail.com>
2024-01-25 09:28:19 -0500

    25_01_2024

---
+++ Semana02/pract07.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char p='p';
+    int x=int(p);
+    cout<<x<<'\t'<<p<<endl;
+    return 0;
+    
+}
---
+++ Semana02/pract08.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    bool var1,
+    //true-false
+    //cualquier valor numerico-0
+    bool var2=100;
+    if(var2){
+        cout<<"TRUE";
+    }else{
+        cout<<"FALSE";
+        return 0;
+}
